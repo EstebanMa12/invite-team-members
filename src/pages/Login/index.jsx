217efdf71eb67c6ea4d08a466b58848ba0542b92
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-08 18:07:57 -0500

    Update dependencies and add login functionality

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,34 +1,53 @@
 import {Link, useNavigate } from 'react-router-dom'
+import { FaGooglePlusG, FaPhone  } from 'react-icons/fa6'
+import { loginGoogle, loginWithEmailAndPassword } from '../../store/users/userThunks'
+import { useDispatch } from 'react-redux'
 
 
 const Login = () => {
+    const dispatch = useDispatch()
     const navigate = useNavigate()
     const handleSubmit = (e) => {
         e.preventDefault()
         navigate('/dashboard')
     }
+    const handleLogin = () =>{
+        dispatch(loginGoogle)
+    }
+    const handleLoginWithEmailAndPassword = (data) => {
+        console.table(data);
+        dispatch(loginWithEmailAndPassword(data))
+      }
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    <h1>Login</h1>
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-3">
-                            <label htmlFor="email" className="form-label">Email address</label>
-                            <input type="email" className="form-control" id="email" />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="password" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="password" />
-                        </div>
-                        <button type="submit" className="btn btn-primary">Login</button>
-                        <Link to="/register" className="btn btn-link">Register</Link>
+        <section className="h-screen">
+            <div className="h-full">
+                <div>
+                    <div><img 
+                    src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                    alt="login"
+                    className="w-full"
+                    />
+                    </div>
+                    <div className="flex flex-row items-center justify-center lg:justify-start">
+                        <p className="mb-0 mr-4 text-lg">
+                            Sign in with
+                        </p>
+                        <button 
+                        className="flex items-center justify-center w-10 h-10 mr-2 bg-gray-200 rounded-full"
+                        onClick={handleLogin}
+                        type='button'
                         
-                        <Link to = "/home" className='btn btn-link'>Home</Link>
-                    </form>
+                        >
+                            <FaGooglePlusG/>
+                        </button>
+                        <button type='button' className="flex items-center justify-center w-5 h-5 bg-gray-200 rounded-full">
+                            <FaPhone/>
+                        </button>
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 export default Login;
---
+++ src/store/users/userThunks.js
@@ -0,0 +1,127 @@
+import {
+    createUserWithEmailAndPassword,
+    updateProfile,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signInWithEmailAndPassword,
+    signOut,
+} from "firebase/auth";
+import { setError, setIsAuthenticate, setUser } from "./userSlice";
+import { auth } from "../../firebase/firebaseConfig";
+import { createUserInCollection } from "../../services/userService";
+
+export const createAnAccountAsync = (newUser) => async (dispatch) => {
+    try {
+        const { user } = await createUserWithEmailAndPassword(
+        auth,
+        newUser.email,
+        newUser.password
+        );
+        await updateProfile(auth.currentUser, {
+        displayName: newUser.name,
+        photoURL: newUser.photoURL,
+        });
+        const userLogged = await createUserInCollection(user.uid, {
+        name: newUser.name,
+        photoURL: newUser.photoURL,
+        accessToken: user.accessToken,
+        email: newUser.email,
+        });
+        // console.log(user)
+        dispatch(
+        setUser({
+            id: userLogged.uid,
+            displayName: userLogged.name,
+            email: userLogged.email,
+            photoURL: userLogged.photoURL,
+            accessToken: userLogged.accessToken,
+        })
+        );
+        dispatch(setIsAuthenticate(true));
+        dispatch(setError(false));
+    } catch (error) {
+        console.warn(error);
+        dispatch(
+        setError({ error: true, code: error.code, message: error.message })
+        );
+    }
+};
+
+export const loginGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    return async (dispatch) => {
+        try {
+        const userCredencial = await signInWithPopup(auth, provider);
+        console.log(userCredencial);
+        dispatch(setIsAuthenticate(true));
+        dispatch(setUser(userCredencial.user));
+        } catch (error) {
+        dispatch(setIsAuthenticate(false));
+        dispatch(
+            setError({ error: true, code: error.code, message: error.message })
+        );
+        }
+    };
+};
+
+export const loginWithEmailAndPassword =
+    ({ email, password }) =>  
+    async (dispatch) => {
+        try {
+        const { user } = await signInWithEmailAndPassword(auth, email, password);
+        console.log(user);
+        dispatch(setIsAuthenticate(true));
+        dispatch(
+            setUser({
+            email: user.email,
+            id: user.uid,
+            name: user.displayName,
+            photoURL: user.photoURL,
+            accessToken: user.accessToken,
+            })
+        );
+        dispatch(setError(false));
+        } catch (error) {
+        dispatch(setIsAuthenticate(false));
+        dispatch(
+            setError({ error: true, code: error.code, message: error.message })
+        );
+        }
+    };
+
+export const logoutAsync = () => async (dispatch) => {
+    try {
+        await signOut(auth);
+        dispatch(setIsAuthenticate(false));
+        dispatch(setUser(null));
+        dispatch(setError(null));
+    } catch (error) {
+        dispatch(
+        setError({ error: true, code: error.code, message: error.message })
+        );
+    }
+};
+
+export const loginWithCodeAsync = (code) => async (dispatch) => {
+    const confirmationResult = window.confirmationResult;
+    try {
+        confirmationResult.confirm(code).then((response) => {
+        const user = response.user.auth.currentUser;
+        console.log(user);
+        dispatch(
+            setUser({
+            email: user.email,
+            id: user.uid,
+            name: user.displayName,
+            photoURL: user.photoURL,
+            accessToken: user.accessToken,
+            })
+        );
+        dispatch(setError(false));
+        });
+    } catch (error) {
+        dispatch(
+        setError({ error: true, code: error.code, message: error.message })
+        );
+    }
+};
