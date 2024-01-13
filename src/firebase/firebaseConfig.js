760e1b900f549590c74d9451ccf83582cc2d2bec
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-13 17:11:00 -0500

    Add Firebase dependencies and update firebaseConfig

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,22 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyANEurwaf3mJmVUnyGRyGPIy4IrLkaN69g",
-  authDomain: "ejercicio-react-redux-firebase.firebaseapp.com",
-  projectId: "ejercicio-react-redux-firebase",
-  storageBucket: "ejercicio-react-redux-firebase.appspot.com",
-  messagingSenderId: "127396799765",
-  appId: "1:127396799765:web:1514afeb49752f3388351c"
+    apiKey: "AIzaSyApYmn4i0A-JWqd0ahqpcJ-GCO7ecGyhVc",
+    authDomain: "invite-team-members.firebaseapp.com",
+    projectId: "invite-team-members",
+    storageBucket: "invite-team-members.appspot.com",
+    messagingSenderId: "853866237924",
+    appId: "1:853866237924:web:f11145e61b762aa0d301f1",
+    measurementId: "G-JXT5BFHSG9"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const firestore = getFirestore(app);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+
