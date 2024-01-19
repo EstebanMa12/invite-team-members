1a298d2808eb90d4e5d72927a601e5cf97c277dc
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-19 15:09:44 -0500

    Add Google and Phone login options

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -69,17 +69,59 @@ const Login = () => {
                     <div className="flex items-center justify-between">
                         <div className="flex items-start">
                             <div className="flex items-center h-5">
-                              <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                                <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
                             </div>
                             <div className="ml-3 text-sm">
-                              <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
                             </div>
                         </div>
                         <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                     </div>
                     <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                    <div className='flex items-center dark:text-gray-300'>
+                        <hr className='grow' />
+                        <span className='m-2'>or</span>
+                        <hr className='grow'/>
+                    </div>
+                    <div className="flex gap-4">
+                        <button
+                            className='
+                            bg-green-400
+                            hover:bg-green-500
+                            text-white h-10
+                            text-sm 
+                            rounded-md my-5 
+                            p-5 flex 
+                            items-center
+                            justify-center
+                            gap-4'
+                            type='button'
+                        >
+                            Ingresar con google
+                            <FaGooglePlusG />
+                        </button>
+                        <button
+                            className='
+                            bg-green-400
+                            hover:bg-green-500
+                            text-white 
+                            text-sm
+                            h-10 
+                            rounded-md 
+                            my-5 p-5 
+                            flex 
+                            items-center
+                            justify-center
+                            gap-4'
+                        
+                            type='button'
+                        >
+                            Ingresar con celular
+                            <FaPhone />
+                        </button>
+                    </div>
                     <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                        Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500"> Sign up</a>
+                        Don’t have an account yet? <Link to="/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500"> Sign up</Link>
                     </p>
                 </form>
             </div>
