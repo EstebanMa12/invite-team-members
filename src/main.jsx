f5c13248bc0460319fb261bed8c7265b504b5d0e
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-19 14:04:44 -0500

    Add react-hook-form and sweetalert2 dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRouter from './router/AppRouter.jsx'
+import './styles/styles.css'
 // import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,52 +1,96 @@
-import {Link, useNavigate } from 'react-router-dom'
-import { FaGooglePlusG, FaPhone  } from 'react-icons/fa6'
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { loginGoogle, loginWithEmailAndPassword } from '../../store/users/userThunks'
-import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
+import { FaGooglePlusG, FaPhone  } from 'react-icons/fa6'
+import Swal from 'sweetalert2'
+import { useEffect } from 'react'
 
 
 const Login = () => {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        navigate('/dashboard')
-    }
-    const handleLogin = () =>{
-        dispatch(loginGoogle)
-    }
-    const handleLoginWithEmailAndPassword = (data) => {
-        console.table(data);
-        dispatch(loginWithEmailAndPassword(data))
-      }
-    return (
-        <section className="h-screen">
-            <div className="h-full">
-                <div>
-                    <div><img 
-                    src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                    alt="login"
-                    className="w-full"
-                    />
-                    </div>
-                    <div className="flex flex-row items-center justify-center lg:justify-start">
-                        <p className="mb-0 mr-4 text-lg">
-                            Sign in with
-                        </p>
-                        <button 
-                        className="flex items-center justify-center w-10 h-10 mr-2 bg-gray-200 rounded-full"
-                        onClick={handleLogin}
-                        type='button'
-                        >
-                            <FaGooglePlusG/>
-                        </button>
-                        <button type='button' className="flex items-center justify-center w-5 h-5 bg-gray-200 rounded-full">
-                            <FaPhone/>
-                        </button>
 
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+//   const dispatch = useDispatch()
+//   const navigate = useNavigate()
+//   const { isAuthenticate, user, error } = useSelector(( store ) => store.user)
+//   const { register, handleSubmit } = useForm()
+
+//   const handleLogin = ()=>{
+//     dispatch(loginGoogle())
+//   }
+  
+
+//   const handleLoginWithEmailAndPassword = (data) => {
+//     console.table(data);
+//     dispatch(loginWithEmailAndPassword(data))
+//   }
+
+//   useEffect(() => {
+//     if (error) {
+//       Swal.fire({
+//         title: "Oops!",
+//         text: "Ha ocurrido un error, por favor verifique sus credenciales",
+//         icon: "Error"
+//       })
+//     }
+  
+//     if ( error === false ) {
+//       Swal.fire({
+//         title: `Bienvenido ${user.name}!`,
+//         text: "Has iniciado sesión exitosamente",
+//         icon: "success"
+//       }).then(navigate('/home'))
+//     }
+//   }, [error])
+
+
+
+  return (<>
+    <section className='w-full px-5'>
+    <h1 className='text-3xl font-bold underline my-3'>Iniciar sesión</h1>
+    <form
+      className='grid justify-start' 
+    >
+      <input 
+          className='border border-indigo-300 rounded-md my-3 h-10 text-indigo-700 px-5' 
+          type='email' 
+          placeholder='Ingrese su correo electrónico'
+          name='email'
+        />
+        <input 
+          className='border border-indigo-300 rounded-md my-3 h-10 text-indigo-700 px-5' 
+          type='password' 
+          placeholder='Ingrese su contraseña' 
+          name='password'
+        />
+        <div className='flex gap-4'>
+          <button className='bg-violet-400 text-white h-10 rounded-md my-5 px-5' type='submit'>Iniciar sesión</button>
+          <button
+            className='bg-green-400 text-white h-10 rounded-md my-5 p-5 flex place-items-center flex gap-2'
+            type='button'
+          >
+            Ingresar con google
+            <FaGooglePlusG />
+          </button>
+          <button
+            className='bg-green-400 text-white h-10 rounded-md my-5 p-5 flex place-items-center flex gap-2'
+            type='button'
+          >
+            Ingresar con celular
+            <FaPhone />
+          </button>
+        </div>
+        <p>
+        Si no tiene una cuenta registrada, registrese
+        <Link 
+          className='text-indigo-900 underline px-1' 
+          to='/register'
+        >
+          aquí
+        </Link>
+      </p>
+    </form>
+  </section>
+  </>)
 }
-export default Login;
+
+export default Login
