40c67bc65ce962dca033a6850963ef2eae45f82f
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-24 14:22:50 -0500

    Add user authentication and state management

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,8 +6,35 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import LoginWithPhone from "../pages/LoginWithPhone";
 import Home from "../pages/Home";
+import { useSelector } from "react-redux";
+import { useState , useEffect} from "react";
+import {onAuthStateChanged} from 'firebase/auth'
+import { setIsAuthenticate, setUser } from "../redux/user/userSlice";
 
 function AppRouter(){
+    const { isAunthenticate, user }= useSelector(store => store.user)
+    const [checking, setChecking] = useState(true)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user?.uid && !user){
+                dispatch(setIsAuthenticate(true))
+                dispatch(setUser({
+                    id: user.uid,
+                    email: user.email,
+                    name: user.displayName,
+                    photoUrl: user.photoURL,
+                    accessToken: user.accessToken
+                }))
+            }
+    })
+    setChecking(false)
+    }, [dispatch, user])
+    if(checking){
+        return <h1>Wait...</h1>
+    }
+
     return(
         <BrowserRouter>
             <Routes>
