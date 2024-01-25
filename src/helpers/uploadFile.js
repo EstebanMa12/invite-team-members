da65917f2514129b5ff3b4e4e94b6c5d99f58823
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-25 15:20:41 -0500

    Refactor cloudinary URL and upload preset

--- src/helpers/uploadFile.js
+++ src/helpers/uploadFile.js
@@ -1,11 +1,16 @@
 import axios from 'axios'
-const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dsgfgsjud/auto/upload'
+
+const cloudName = 'dsgfgsjud'
+const uploadPreset = 'TechnicalTest'
+const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/${cloudName}/auto/upload`
+
+
 
 export const saveImage = async file =>{
     const body={
         file,
         api_key: 523377193467897,
-        upload_preset: 'ml_default'
+        upload_preset: uploadPreset
     }
     const headers = {
         'Content-Type': 'multipart/form-data',
