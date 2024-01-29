44c9b0cc8dd6fe7a8af41b66ada65c9fcd52296e
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-29 05:21:03 -0500

    Remove attributesSlice.js and attributesThunks.js

---
+++ src/redux/guest/guestSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    guest: null,
+    error: null
+}
+
+const guestSlice =createSlice({
+    name: "guest",
+    initialState,
+    reducers: {
+        setGuest: (state, action) => {
+            state.guest = action.payload
+        },
+        setError: (state, action) => {
+            state.error = action.payload
+        },      
+
+    },
+});
+
+export const { setGuest, setError } = guestSlice.actions;
+export default guestSlice.reducer;
+
+
+
